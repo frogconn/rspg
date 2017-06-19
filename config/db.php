268cad828b6f47575b6f155df70878eb4f29b946
AniamK <kathris@hotmail.co.th>
2017-06-19 14:01:44 +0700

    edit config/db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=rspg_db',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=10.101.109.103;port=3333;dbname=rspg_db',
+    'username' => 'rspg',
+    'password' => 'dfoYi2nBGI69M8hs',
     'charset' => 'utf8',
 ];
