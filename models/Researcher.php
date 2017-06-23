be2e5ecc3a5c9535a3d14977c4a9c4200b54f69a
ballapilak <ball.apilak13@gmail.com>
2017-06-23 16:32:47 +0700

    update

--- models/Researcher.php
+++ models/Researcher.php
@@ -3,21 +3,17 @@
 namespace app\models;
 
 use Yii;
-//use yii\db\BaseActiveRecord;
-use yii\web\UploadedFile;
-//use yii\db\Expression;
+
 /**
  * This is the model class for table "researcher".
  *
  * @property string $id
  * @property integer $foreigner
- * @property string $institution_id
- * @property string $faculty_id
  * @property string $pers_id
  * @property string $title
- * @property string $fristname_th
+ * @property string $firstname_th
  * @property string $lastname_th
- * @property string $fristname_en
+ * @property string $firstname_en
  * @property string $lastname_en
  * @property string $fullname_th
  * @property string $fullname_en
@@ -29,17 +25,12 @@ use yii\web\UploadedFile;
  * @property integer $created_by
  * @property string $created_date
  * @property integer $update_by
- *
- * @property Evidence[] $evidences
  */
 class Researcher extends \yii\db\ActiveRecord
 {
     /**
      * @inheritdoc
      */
-	 
-	public $upload_foler ='uploads\researcher_evidence';
-	 
     public static function tableName()
     {
         return 'researcher';
@@ -51,16 +42,12 @@ class Researcher extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['foreigner', 'institution_id', 'faculty_id', 'gender', 'created_by', 'update_by'], 'integer'],
+            [['foreigner', 'gender', 'created_by', 'update_by'], 'integer'],
             [['update_date', 'created_date'], 'safe'],
             [['pers_id', 'telephone'], 'string', 'max' => 64],
             [['title'], 'string', 'max' => 32],
-            [['fristname_th', 'lastname_th', 'fristname_en', 'lastname_en', 'email'], 'string', 'max' => 128],
-            [['fullname_th', 'fullname_en'], 'string', 'max' => 255],
-			//[['evidence_file'],'required'],
-			[['evidence_file'], 'file',
-			'skipOnEmpty' => true,
-			'extensions' => 'png,jpg'],
+            [['firstname_th', 'lastname_th', 'firstname_en', 'lastname_en', 'email'], 'string', 'max' => 128],
+            [['fullname_th', 'fullname_en', 'evidence_file'], 'string', 'max' => 255],
         ];
     }
 
@@ -72,85 +59,22 @@ class Researcher extends \yii\db\ActiveRecord
         return [
             'id' => 'รหัสนักวิจัย',
             'foreigner' => 'ชาวต่างชาติ',
-            'institution_id' => 'รหัสสถาบัน',
-            'faculty_id' => 'รหัสคณะ',
             'pers_id' => 'หมายเลขบัตรประชาชน/หมายเลขหนังสือเดินทาง',
             'title' => 'คำนำหน้าชื่อ',
-            'fristname_th' => 'ชื่อ(ไทย)',
+            'firstname_th' => 'ชื่อ(ไทย)',
             'lastname_th' => 'นามสกุล(ไทย)',
-            'fristname_en' => 'ชื่อ(อังกฤษ)',
+            'firstname_en' => 'ชื่อ(อังกฤษ)',
             'lastname_en' => 'นามสกุล(อังกฤษ)',
             'fullname_th' => 'ชื่อเต็ม(ไทย)',
             'fullname_en' => 'ชื่อเต็ม(อังกฤษ)',
             'gender' => 'เพศ',
             'email' => 'อีเมล',
             'telephone' => 'เบอร์โทรศัพท์',
-            'evidence_file' => 'Evidence File',
+            'evidence_file' => 'ไฟล์หลักฐาน',
             'update_date' => 'Update Date',
             'created_by' => 'Created By',
             'created_date' => 'Created Date',
             'update_by' => 'Update By',
         ];
     }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getEvidences()
-    {
-        return $this->hasMany(Evidence::className(), ['researcher_id' => 'id']);
-    }
-	
-	//
-	public function upload($model,$attribute)
-	{
-		$evidence_file  = UploadedFile::getInstance($model, $attribute);
-		$path = $this->getUploadPath();
-		if ($this->validate() && $evidence_file !== null) 
-		{
-			$fileName = md5($evidence_file->baseName.time()) . '.' . $evidence_file->extension;
-			//$fileName = $picture->baseName . '.' . $picture->extension;
-			if($evidence_file->saveAs($path.$fileName))
-			{
-				return $fileName;
-			}
-		}
-		return $model->isNewRecord ? false : $model->getOldAttribute($attribute);
-	}
-
-	public function getUploadPath()
-	{
-		return Yii::getAlias('@webroot').'/'.$this->upload_foler.'/';
-	}
-
-	public function getUploadUrl()
-	{
-		return Yii::getAlias('@web').'/'.$this->upload_foler.'/';
-	}
-
-	public function getPhotoViewer()
-	{
-		return empty($this->evidence_file) ? Yii::getAlias('@web').'/img/none.png' : $this->getUploadUrl().$this->evidence_file;
-	}
-    /*
-    public function afterSave()
-    {
-        $this->fullname_th = $this->fristname_th." ".$this->lastname_th;
-        $this->fullname_en = $this->fristname_en." ".$this->lastname_en;
-    }
-    */
-    public function beforeSave($insert)
-    {
-        if (parent::beforeSave($insert)) 
-        {
-            if($this->isNewRecord)
-            {
-                $this->created_date = new \yii\db\Expression('NOW()');
-            }
-             $this->update_date = new \yii\db\Expression('NOW()');
-            return true;
-        }
-        return false;
-    }
- 
 }
--- models/ResearcherSearch.php
+++ models/ResearcherSearch.php
@@ -19,7 +19,7 @@ class ResearcherSearch extends Researcher
     {
         return [
             [['id', 'foreigner', 'institution_id', 'faculty_id', 'gender', 'created_by', 'update_by'], 'integer'],
-            [['pers_id', 'title', 'fristname_th', 'lastname_th', 'fristname_en', 'lastname_en', 'fullname_th', 'fullname_en', 'email', 'telephone', 'update_date', 'created_date'], 'safe'],
+            [['pers_id', 'title', 'firstname_th', 'lastname_th', 'firstname_en', 'lastname_en', 'fullname_th', 'fullname_en', 'email', 'telephone', 'update_date', 'created_date'], 'safe'],
         ];
     }
 
@@ -72,9 +72,9 @@ class ResearcherSearch extends Researcher
 
         $query->andFilterWhere(['like', 'pers_id', $this->pers_id])
             ->andFilterWhere(['like', 'title', $this->title])
-            ->andFilterWhere(['like', 'fristname_th', $this->fristname_th])
+            ->andFilterWhere(['like', 'firstname_th', $this->firstname_th])
             ->andFilterWhere(['like', 'lastname_th', $this->lastname_th])
-            ->andFilterWhere(['like', 'fristname_en', $this->fristname_en])
+            ->andFilterWhere(['like', 'firstname_en', $this->firstname_en])
             ->andFilterWhere(['like', 'lastname_en', $this->lastname_en])
             ->andFilterWhere(['like', 'fullname_th', $this->fullname_th])
             ->andFilterWhere(['like', 'fullname_en', $this->fullname_en])
