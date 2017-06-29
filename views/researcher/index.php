36ba69a8296bb2597dcb28f6723424621618d640
ballapilak <ball.apilak13@gmail.com>
2017-06-29 09:22:11 +0700

    update

--- views/researcher/index.php
+++ views/researcher/index.php
@@ -29,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
+        'filterModel' => $searchresearcher,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn','options'=> ['style'=>'width:50px;']],
 
