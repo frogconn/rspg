bed17a375efa2fb6e7d1f75515bc2f9ce5a9098a
ballapilak <ball.apilak13@gmail.com>
2017-06-30 15:50:46 +0700

    fix getcode

--- controllers/ResearcherController.php
+++ controllers/ResearcherController.php
@@ -56,9 +56,27 @@ class ResearcherController extends Controller
      */
     public function actionView($id)
     {
+        $foreigner_value = $model->getCode($model->is_foreigner);
         return $this->render('view', [
             'model' => $this->findModel($id),
+            'foreigner_value'=>$foreigner_value,
+
         ]);
+    }
+    protected static function getCode($id){
+        if($id=='Y')
+        {
+            return 'ใช่';
+        }elseif($id=='N') {
+            return 'ไม่ใช่';
+        }
+        elseif($id=='M')
+        {
+            return 'ชาย';
+        }
+        else{
+            return 'หญิง';
+        }
     }
 
     /**
