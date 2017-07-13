17f1ccce701abb4e114c803e37c9943601b49eb4
Viewz <chanyanood.c@kkumail.com>
2017-07-13 16:15:41 -0700

    frontend asset

---
+++ assets/FrontendAsset.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class FrontendAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        //'css/site.css',
+        'themes/frontend/css/bootstrap.css',
+        'themes/frontend/css/bootstrap.min.css',
+        'themes/frontend/css/clean-blog.min.css',
+        'themes/frontend/css/clean-blog.css',
+    ];
+    public $js = [
+        //'js/yii_overrides.js',
+        'themes/frontend/js/bootstrap.js',
+        'themes/frontend/js/bootstrap.min.js',
+        'themes/frontend/js/clean-blog.js',
+        'themes/frontend/js/clean-blog.min.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'app\assets\SweetAlertAsset',
+    ];
+}
