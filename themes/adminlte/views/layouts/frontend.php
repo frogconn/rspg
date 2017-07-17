9b6b07e93d6c56cea163bf8fc367244db6fca209
mewmallika <mewmallika0410@gmail.com>
2017-07-17 10:25:42 +0700

    font index

--- themes/adminlte/views/layouts/frontend.php
+++ themes/adminlte/views/layouts/frontend.php
@@ -16,7 +16,6 @@ FrontendAsset::register($this);
     <head>
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
