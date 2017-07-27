b1b61f48e7ddaad80ec3617264d7fb63c4ecb8f1
ballapilak <ball.apilak13@gmail.com>
2017-07-27 16:15:33 +0700

    add boxข้อมูลทั่วไป

--- views/site/admin.php
+++ views/site/admin.php
@@ -135,5 +135,47 @@ $this->title = '';
 		<!-- /.box-footer -->
 	</div></div>
 
+<div class="row">
+	<div class="col-md-3">
+	<div class="box box-success">
+		<div class="box-header with-border">
+			<h3 class="box-title">ข้อมูลทั่วไป</h3>
+			<div class="box-tools pull-right">
+				<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+				<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+			</div>
+		</div>
+		<!-- /.box-header -->
+		<div class="box-body">
+			<ul class="products-list product-list-in-box">
+				<li class="item">
+					<div class="product-img">
+						<?= Html::img('img\none.png',['class'=>'img-thumbnail','style'=>'width:48px;height:48px']);?>
+					</div>
+					<div class="product-info">
+						<?= Html::a('ข้อมูลคณะ', ['/researcher-faculty'], ['class' => 'profile-link']) ?>
+						<span class="product-description"></span>
+					</div>
+				</li>
+				<!-- /.item -->
+				<li class="item">
+					<div class="product-img">
+						<?= Html::img('img\none.png',['class'=>'img-thumbnail','style'=>'width:48px;height:48px']);?>
+					</div>
+					<div class="product-info">
+						<?= Html::a('ข้อมูลมหาวิทยาลัย', ['/researcher-institution']) ?>
+						<span class="product-description"></span>
+					</div>
+				</li>
+				<!-- /.item -->
+			</ul>
+		</div>
+		<!-- /.box-body -->
+		<!--div class="box-footer text-center">
+		<a href="javascript:void(0)" class="uppercase">View All Products</a>
+		</di-->
+		<!-- /.box-footer -->
+	</div></div>
+
 	<div class="clearfix"></div>
 </div>
