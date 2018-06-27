bed22320b5defad0f60e79a2cc55cf347d60eb09
rnd-pai <ksorn@kku.ac.th>
2018-06-27 13:19:54 +0700

    no message

--- views/resource-micrology/view.php
+++ views/resource-micrology/view.php
@@ -36,12 +36,12 @@ $this->params['breadcrumbs'][] = $this->title;
             </div>
         </div>
     </header>
-    <nav class="navbar navbar-light" style="background-color: #8B0BE1;">
+    <!-- <nav class="navbar navbar-light" style="background-color: #8B0BE1;">
     <marquee direction="left"><font color="#ffff33"><B><h4> โครงการอนุรักษ์พันธุกรรมพืชอันเนื่องมาจากพระราชดำริ
     ฐานข้อมูลการดำเนินงานหน่วยงานร่วมสนองพระราชดำริฯ<h4><B></font></marquee>
     <div class="collapse navbar-collapse" id="navbarColor01">
     </div>
-    </nav>
+    </nav> -->
 
     <style>
       body {
